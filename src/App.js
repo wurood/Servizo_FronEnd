3c691a7113e1f7efb17a2dea3dc15f1250d16ede
unknown <wuroodj95@gmail.com>
2023-05-26 13:13:26 +0300

    add shope hero without cards

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css'
 import { MDBContainer } from 'mdb-react-ui-kit';
 import {Logo} from './components/Logo'
 import { SignUP } from './container/SignUP';
-import {AboutUs}from './components/AboutUs'
+import {ShopeHero}from './components/ShopeHero'
 function App() {
   return (
     <MDBContainer fluid>
-      <AboutUs />
+      <ShopeHero />
     </MDBContainer>
   );
 }
---
+++ src/components/ShopeHero/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './style.module.css'
+
+export const ShopeHero = () => {
+  return (
+    <div className={style.hero}>
+       <p className={style.tittle}>
+        Top  Shopes
+        <span className={style.border}></span>
+      </p>
+      <div className={style.cards_wrapper}>
+
+      </div>
+    </div>
+  )
+}
