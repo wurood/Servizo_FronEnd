827fd297cf969bc0aa8188b0d1cf08daf870d6f4
unknown <wuroodj95@gmail.com>
2023-05-28 01:39:25 +0300

    enhance Request form

--- src/components/CheckAccount/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-
-export const CheckAccount = (props) => {
-  return (
-    <p   className={` ${style.text} ${props.class}`} >{props.text}<span className={style.span}>  {props.navigate}</span></p>
-  )
-}
--- src/components/SideImage/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-import Image from '../../assets/Side_Image.png'
-
-export const SideImage = () => {
-  return (
-    <img src={Image} alt="side image" className={style.sideImage}/>
-  )
-}
