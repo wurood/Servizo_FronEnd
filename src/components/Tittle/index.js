2c8c191e307c6497b3008a4260b9cb9a0e732b18
unknown <wuroodj95@gmail.com>
2023-05-27 18:07:38 +0300

    enhance about us

--- src/components/Tittle/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-
-export const Tittle = (props) => {
-  return (
-    <h1 className={ `${style.text} ${props.class} `}>{props.Tittle}</h1>
-  )
-}
