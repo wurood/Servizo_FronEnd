c80dd7366c9555f646b4d804b760447e8fac337e
unknown <wuroodj95@gmail.com>
2023-05-27 18:44:03 +0300

    enhance cards

--- src/components/Logo/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-import LogoImage from '../../assets/logo.png'
-
-export const Logo = (props) => {
-  return (
-    <img src={LogoImage} alt='logo image' className={` ${style.logo} ${props.class}`}/>
-  )
-}
