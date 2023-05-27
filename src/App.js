dba90defe3a4f6c9a0fae0d3ad2fe9677d536a07
unknown <wuroodj95@gmail.com>
2023-05-27 20:35:16 +0300

    enhance description section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
 import { MDBContainer } from 'mdb-react-ui-kit';
-import {Logo} from './components/Logo'
-import { SignUP } from './container/SignUP';
 import {ContactUs}from './components/ContactUs'
 function App() {
   return (
--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -5,7 +5,12 @@ import {SubTittle} from '../SubTittle/index'
 import { MDBRow, MDBCol,MDBInput  } from 'mdb-react-ui-kit';
 import {Button} from '../../components/Button'
 import Map from '../../assets/map.png'
-
+import { AiFillPhone } from "react-icons/ai";
+import { VscLocation } from "react-icons/vsc";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillYoutube} from "react-icons/ai";
+import { AiFillInstagram} from "react-icons/ai";
 export const ContactUs = () => {
     
   const [formData, setFormData] = useState({
@@ -33,12 +38,12 @@ export const ContactUs = () => {
     <div className={style.wrapper}>
       <Tittle Tittle="Get In Touch"  class={style.Tittle}/>
       <MDBRow className={style.row}>
-      <MDBCol lg='6' md='6' sm='6' xs='12'>
+      <MDBCol lg='6' md='6' xs='12' sm='6'  className={style.colHeight}>
       <SubTittle Tittle=" Leave us a message"  class={style.SubTittle}/>
 
           <MDBRow tag="form" className={ ` ${style.form}`} onSubmit={(e) =>handelSubmit(e)}>
           {MessageFormeData.map((item) =>(
-                <MDBCol md="10">
+                <MDBCol md="10" >
                 <MDBInput
                     value={item.value}
                     name={item.data}
@@ -57,9 +62,18 @@ export const ContactUs = () => {
           </MDBRow>
 
       </MDBCol>
-      <MDBCol lg='6' md='6' sm='6'  className={style.sideImage} >
-      <img alt="Map" src={Map} className={style.Map} />
-
+      <MDBCol lg='6' md='6'  xs='12' sm='6' className={style.colHeight} >
+      <div className={style.text}>
+                <p><VscLocation />Infomation technologies building,<br />Victoria Island, Lagos, Nigeria.</p>
+                <p><AiFillPhone /> (123) 456-7890</p>
+                <div className='social-media'>
+                    <AiFillYoutube/>
+                    <AiFillInstagram/>
+                    <AiFillFacebook />
+                    <AiFillTwitterSquare />
+                </div>
+                <img alt="Map" src={Map} className={style.Map} />
+            </div> 
       </MDBCol>
     </MDBRow>
     </div>
