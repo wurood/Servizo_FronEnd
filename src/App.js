1be0f8f7c9a6e87e8b1b6126961b1ce485e02bb1
unknown <wuroodj95@gmail.com>
2023-05-27 17:13:33 +0300

    enhance hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
 import { MDBContainer } from 'mdb-react-ui-kit';
-import {Logo} from './components/Logo'
-import { SignUP } from './container/SignUP';
 import {Hero}from './components/Hero'
 function App() {
   return (
--- src/components/Language/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import style from './style.module.css'
-
-export const Language = () => {
-  return (
-    <div className={style.text}>EN | AR </div>
-  )
-}
-
