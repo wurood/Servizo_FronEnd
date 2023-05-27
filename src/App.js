7a1f030dff905ab38bbad7f1f184246f462d540c
unknown <wuroodj95@gmail.com>
2023-05-27 15:12:19 +0300

    enhance footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
 import { MDBContainer } from 'mdb-react-ui-kit';
-import {Logo} from './components/Logo'
-import { SignUP } from './container/SignUP';
 import Footer from './components/Footer'
 function App() {
   return (
